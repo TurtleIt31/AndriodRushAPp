81153dcb52644dbe214954eacc1cf34940792988
TurtleIt31 <brazenthree9@gmail.com>
2024-11-15 12:21:49 +0200

    added some basic UI with code, not tested, added data models for some of the tables in the database

--- app/src/main/java/com/example/rushapp/UserTableAdapter.kt
+++ app/src/main/java/com/example/rushapp/UserTableAdapter.kt
@@ -1,12 +1,13 @@
 package com.example.rushapp
 
-import Data.Models.User
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.rushapp.data.model.User
 
 class UserTableAdapter(
     private val userList: List<User>, // List of user data
